ac8b288ff72fc6ea3f7914248cef82a8fce33c46
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-08 11:30:33 -0800

    Export auth,provider, and db.

--- src/firebase.js
+++ src/firebase.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
 const analytics = getAnalytics(app);
