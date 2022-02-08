54844d04180c21b89a065bd20e49a1172acd66cc
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-08 11:21:06 -0800

    Add the sign in function to the button upon clicking it.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,16 +4,17 @@ import{signInWithPopup} from 'firebase/auth'
 function Login({setIsAuth}) {
   const signInWithGoogle = () => {
     signInWithPopup(auth, provider).then((result) => {
-      localStorage.setItem("isAuth", true)
-      setIsAuth(true)
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
     });
   };
-  return 
-  <div className='LoginPage'>
-    <p>Sign in with google to continue</p>
-    <button className="login-with-google-btn"> 
-    Sign in with Google</button>
-  </div>;
+  return (
+    <div className='LoginPage'>
+      <p>Sign in with google to continue</p>
+      <button className="login-with-google-btn" onClick={signInWithGoogle}> 
+      Sign in with Google</button>
+    </div>
+  );
 }
 
 export default Login;
