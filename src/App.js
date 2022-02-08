0652c0a07f600b50edf604fe21a2e44c0362523c
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-08 10:58:18 -0800

    Add routes for each pages.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import Home from './pages/Home';
+import CreatePost from './pages/CreatePost';
+import Login from './pages/Login';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/createPost" element={<CreatePost/>}/>
+        <Route path="/login" element={<CreatePost/>}/>
       </Routes>
     </Router>
   );
