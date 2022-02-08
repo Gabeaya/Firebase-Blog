a4049980b094bbb9c1e3ce590b73b08ee3f18878
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-08 11:13:56 -0800

    Add the pop up signup from firebase.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,18 @@
 import React from 'react';
-
+import {auth, provider} from '../firebase'
+import{signInWithPopup} from 'firebase/auth'
 function Login() {
-  return <div></div>;
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      
+    })
+  }
+  return 
+  <div className='LoginPage'>
+    <p>Sign in with google to continue</p>
+    <button className="login-with-google-btn"> 
+    Sign in with Google</button>
+  </div>;
 }
 
 export default Login;
