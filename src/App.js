ea4698956d1325e5a742b7dfbf4ccf4c1f18526a
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-08 10:37:17 -0800

    Import Home view.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Home from './pages/Home';
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={}/>
-
-
+        <Route path="/" element={<Home/>}/>
       </Routes>
     </Router>
   );
