76731fb6cb21d0f6f4dad3462718f94ba1e30208
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-08 20:39:23 -0800

    Add a return that will display list posts.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,18 +2,35 @@ import React, { useEffect, useState } from 'react';
 import {getDocs, collection} from 'firebase/firestore';
 import {db} from '../firebase';
 function Home() {
-  const [postsLists, setPostsLists] = useState([]);
+  const [postList, setPostList] = useState([]);
   const postsCollectionRef = collection(db, "posts")
 
   useEffect(() => {
     const getPosts = async () => {
       const data = await getDocs(postsCollectionRef);
-      console.log(data); 
+      setPostList(data.docs.map((doc) => ({...doc.data(), id: doc.id}))); 
+      console.log(data.docs.map((doc) => ({...doc.data(), id: doc.id}))); 
     };
 
     getPosts();
   })
-  return <div className='homePage'> </div>;
+  return (
+    <div className='homePage'>
+      {postList.map((post)=> {
+        return (
+          <div className='post'> 
+
+            <div className='postHeader'> 
+
+              <div className='title'> <h1> {post.tile}</h1></div>
+            </div>
+            <div className='postTextContainer'> {post.postText}</div>
+            <h3>@{post.author.name}</h3>
+          </div>
+        );
+      })} 
+    </div>
+  );
 }
 
 export default Home;
