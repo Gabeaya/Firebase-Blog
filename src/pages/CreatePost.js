3464edd412fab6c2eae571e1221e94e300358bbe
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-08 19:43:40 -0800

    update creat post function to navigate to the home page after posting.

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,16 +1,20 @@
 import React, {useState} from 'react';
 import { addDoc, collection } from 'firebase/firestore'; //allows to add a doc to the database table
 import { db, auth } from '../firebase';
+import { useNavigate } from "react-router-dom";
 
 function CreatePost() {
   const [title, setTitle] = useState("");//collects user input for title
   
   const [postText, setPostText] = useState("");//collects user input for post info
   const postsCollectionRef = collection(db, "posts")
-
+  let navigate = useNavigate();
   const createPost = async () => {
-    await addDoc(postsCollectionRef, {title, postText, author: {name: auth.currentUser.displayName, id: auth.currentUser.uid}})
-  }
+    await addDoc(postsCollectionRef, {title, postText, author: {name: auth.currentUser.displayName, id: auth.currentUser.uid},
+    });
+    navigate("/");
+  };
+
   return (
     <div className="createPostPage">
       <div className='cpContainer'>  
