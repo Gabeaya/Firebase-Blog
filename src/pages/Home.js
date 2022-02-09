d57fe8388b5f995cbdb076fd25ca109175dfb902
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-08 20:15:19 -0800

    Add a  use effect to get the docs from user inpouts and include the use state to store all the posts.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import {getDoc, getDocs} from 'firebase/firestore';
 function Home() {
-  return <div>Home</div>;
+  const [postsLists, setPostsLists] = useState([]);
+  const postsCollectionRef = collection(db, "posts")
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+    }
+  })
+  return <div className='homePage'> </div>;
 }
 
 export default Home;
