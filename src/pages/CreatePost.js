add5d10382c38e08af41dbacaed28a7b78dde4fc
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-08 19:19:59 -0800

    Add a function that takes the users info and adds a post to their profile.

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,21 +1,33 @@
 import React, {useState} from 'react';
+import { addDoc, collection } from 'firebase/firestore'; //allows to add a doc to the database table
+import { db, auth } from '../firebase';
 
 function CreatePost() {
-  const [title, setTitle] = useState("");
-  const [postText, setPostText] = useState("");
+  const [title, setTitle] = useState("");//collects user input for title
+  
+  const [postText, setPostText] = useState("");//collects user input for post info
+  const postsCollectionRef = collection(db, "posts")
 
+  const createPost = async () => {
+    await addDoc(postsCollectionRef, {title, postText, author: {name: auth.currentUser.displayName, id: auth.currentUser.uid}})
+  }
   return (
     <div className="createPostPage">
       <div className='cpContainer'>  
         <h1> Create Post </h1>
         <div className='inputGp'>
           <label> Title:</label>
-          <input placeholder='Title..'/>
+          <input placeholder='Title..' onChange={(event) => {
+            setTitle(event.target.value);
+            }}
+          />
         </div>
 
         <div className='inputGp'>
           <label> Post:</label>
-          <textarea placeholder='Post' />
+          <textarea placeholder='Post' onChange={(event) => {
+            setPostText(event.target.value);
+          }}/>
         </div>
         <button> Submit Post</button>
       </div>
